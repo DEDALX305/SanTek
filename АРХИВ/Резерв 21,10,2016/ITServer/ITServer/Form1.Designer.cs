33f87ebd6c080b4b51de40860a067de60858591d
DEDALX305 <dedalx305@mail.ru>
2016-10-21 16:39:12 +0500

    Создана резервная копия

---
+++ АРХИВ/Резерв 21,10,2016/ITServer/ITServer/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace ITServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(1133, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(139, 693);
+            this.listBox1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(886, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(241, 698);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1284, 722);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ АРХИВ/Резерв 21,10,2016/ITServer/ITServer/ServerHandler.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ITMobileClientPrototype;
+using System.Drawing;
+
+namespace ITServer
+{
+    [Serializable]
+    public class ServerHandler : MarshalByRefObject
+    {
+
+        public List<Player> playersList = new List<Player>();
+        public List<string> messagesList = new List<string>();
+        public string private_hash = "337711732STOP|";
+
+        private bool isNearby(Player p1, Player p2)
+        {
+            if (Math.Sqrt((p2.getX() - p1.getX()) * (p2.getX() - p1.getX()) + (p2.getY() - p1.getY()) * (p2.getY() - p1.getY())) < 5)
+                return true;
+            else
+                return false;
+
+        }
+
+        public bool checkCredentials(string email, string password)
+        {
+            return true;
+        }
+
+        public int getMaxX()
+        {
+            Image m = Image.FromFile("map.jpg");
+            return m.Width;
+        }
+
+        public int getMaxY()
+        {
+            Image m = Image.FromFile("map.jpg");
+            return m.Height;
+        }
+
+        public void addMessage(string s){
+            messagesList.Add(s);
+        }
+
+        public void deletePlayer(string email)
+        {
+            int x = -1;
+
+            for (int i = 0; i < playersList.Count; i++)
+            {
+                if (playersList[i].getEmail() == email) x = i;
+            }
+
+            playersList.RemoveAt(x);
+        }
+
+        public string[] getNearbyPlayers(string email)
+        {
+            List<string> l = new List<string>();
+            
+            Player p1 = null;
+
+            foreach (Player p in playersList)
+            {
+                if (p.getEmail() == email) p1 = p;
+            }
+            foreach (Player p in playersList)
+            {
+                if (p.getEmail() != email && isNearby(p, p1))
+                l.Add(p.getEmail());
+            }
+            return l.ToArray();
+        }
+
+        public string[] getMessages(string email)
+        {
+            List<string> l = new List<string>();
+            foreach (string s in messagesList)
+            {
+                if (s.StartsWith(private_hash)){
+                    string[] lines = s.Split('|');
+                    string whom = lines[1];
+                    if (email == whom)
+                    {
+                        l.Add(lines[2]);
+                    }
+                }
+                else
+
+                l.Add(s);
+            }
+            return l.ToArray();
+        }
+
+        public void addPlayer(string email)
+        {
+            playersList.Add(new Player(email));
+            
+        }
+
+        public void sendMessageTo(string who, string whom)
+        {
+            //messagesList.Add(p.getEmail() + ":" + s);
+
+        }
+
+        public void changePlayerState(string email, int x, int y)
+        {
+            foreach (Player p in playersList)
+            {
+                if (p.getEmail() == email)
+                {
+                    p.setCoordinates(x, y);
+                }
+            }
+        }
+
+         
+    }
+}
